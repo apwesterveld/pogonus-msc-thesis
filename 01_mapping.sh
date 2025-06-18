42e6eb1cd342c68a57bd7b10e19a98bf80afb25d
apwesterveld <161853490+apwesterveld@users.noreply.github.com>
2025-06-18 21:34:17 +0200

    Update 01_mapping.sh

--- 01_mapping.sh
+++ 01_mapping.sh
@@ -63,7 +63,7 @@ FILE2=/scratch/leuven/361/vsc36175/Pogonus_RAD_Nieuwpoort_Sander_18062025/$(echo
 mkdir -p $BWAout
 
 # Map reads using bwa mem
-bwa mem -t 20 -M $REF $FILE1 $FILE2 | samtools view -bS - > $BWAout/$(echo "${samples[ID]}").$REFNAME.bam
+bwa mem -t 20 -M $REF $FILE1 $FILE2 | samtools view -bS - > $BWAout/${samples[ID]}.$REFNAME.bam
 
 # Alternative, map with minimap2
 #minimap2 -ax sr -t 20 $REF $file1 $file2 | samtools view -bS - > $minimapOUT/$(echo "${samples[ID]}").$REFNAME.bam
