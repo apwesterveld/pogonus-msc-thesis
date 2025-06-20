0588c912bf7bb951d58cad8a6d8b0783e1994f57
apwesterveld <161853490+apwesterveld@users.noreply.github.com>
2025-06-20 10:56:33 +0200

    Update 04_merge_vcf.sh

--- 04_merge_vcf.sh
+++ 04_merge_vcf.sh
@@ -7,9 +7,12 @@
 #SBATCH -A lp_svbelleghem
 #SBATCH -o merge_vcfs.%j.out
 
-module load Python/3.7.0-foss-2018a
-export BCFTOOLS_PLUGINS=/data/leuven/361/vsc36175/bcftools/plugins
+module load BCFtools/1.9-foss-2018a
+
+# For local bcftools install:
+#module load Python/3.7.0-foss-2018a
+#export BCFTOOLS_PLUGINS=/data/leuven/361/vsc36175/bcftools/plugins
 
 cd /scratch/leuven/361/vsc36175/
 
-bcftools concat -Oz -o merged_variants.vcf.gz *.vcf.gz
+bcftools concat -Oz -o P_chalceus_NP25_BarSW_merged.vcf.gz *.vcf.gz
