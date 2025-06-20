2b67716ca48f1927caf061c88ac437c16d0da376
apwesterveld <161853490+apwesterveld@users.noreply.github.com>
2025-06-20 11:32:24 +0200

    Create 04c_stats.sh

---
+++ 04c_stats.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l 
+#SBATCH --cluster=genius 
+#SBATCH --job-name stats 
+#SBATCH --nodes=1 
+#SBATCH --cpus-per-task=20
+#SBATCH --time=24:00:00 
+#SBATCH -A lp_svbelleghem
+#SBATCH -o stats.%j.out
+
+module load BCFtools/1.9-foss-2018a
+
+source /data/leuven/361/vsc36175/miniconda3/etc/profile.d/conda.sh
+conda activate vcftools
+
+# Gives variant count, transition/transversion ratio, missing genotype rates, etc..
+bcftools stats P_chalceus_NP25_BarSW_merged.vcf.gz
+
+# Gives a brief summary of stats
+bcftools stats P_chalceus_NP25_BarSW_merged.vcf.gz | grep -E "SN|TSTV"
+
+# Counts the number of variants (excluding the header).
+bcftools view -H P_chalceus_NP25_BarSW_merged.vcf.gz | wc -l
+
+# Checks missingness
+vcftools --gzvcf P_chalceus_NP25_BarSW_merged.vcf.gz --missing-indv
+
+# Checks allele frequency distribution 
+vcftools --gzvcf P_chalceus_NP25_BarSW_merged.vcf.gz --freq
