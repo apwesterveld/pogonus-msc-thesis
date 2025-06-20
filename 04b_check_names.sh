7468934536535d05b3a9243deca3e4f699b5ca2d
apwesterveld <161853490+apwesterveld@users.noreply.github.com>
2025-06-20 11:10:26 +0200

    Update 04b_check_names.sh

--- 04b_check_names.sh
+++ 04b_check_names.sh
@@ -2,14 +2,13 @@
 #SBATCH --cluster=genius 
 #SBATCH --job-name check_names 
 #SBATCH --nodes=1 
-#SBATCH --ntasks-per-node=20
-#SBATCH --time=72:00:00 
+#SBATCH --cpus-per-task=20
+#SBATCH --time=24:00:00 
 #SBATCH -A lp_svbelleghem
 #SBATCH -o check_names.%j.out
 
-module load Python/3.7.0-foss-2018a
-export BCFTOOLS_PLUGINS=/data/leuven/361/vsc36175/bcftools/plugins
+module load BCFtools/1.9-foss-2018a
 
-bcftools query -l merged_variants.vcf.gz > vcf_samples.txt
+bcftools query -l P_chalceus_NP25_BarSW_merged.vcf.gz > vcf_samples.txt
 cut -f2 phenotype_modified.txt  > phenotype_samples.txt
 comm -3 <(sort vcf_samples.txt) <(sort phenotype_samples.txt)
