f0f184839ee5a395f73c25da15e9fc5036c975de
apwesterveld <161853490+apwesterveld@users.noreply.github.com>
2025-06-20 11:25:27 +0200

    Update 05_filter_vcf.sh

--- 05_filter_vcf.sh
+++ 05_filter_vcf.sh
@@ -2,23 +2,25 @@
 #SBATCH --cluster=genius 
 #SBATCH --job-name filter 
 #SBATCH --nodes=1 
-#SBATCH --ntasks-per-node=20
-#SBATCH --time=72:00:00 
+#SBATCH --cpus-per-task=20
+#SBATCH --time=24:00:00 
 #SBATCH -A lp_svbelleghem
 #SBATCH -o filter_vcf.%j.out
 
-source /data/leuven/357/vsc35707/miniconda3/etc/profile.d/conda.sh
+source /data/leuven/361/vsc36175/miniconda3/etc/profile.d/conda.sh
 conda activate vcftools
 module load tabix/0.2.6-GCCcore-6.4.0
 
 # Check missingness per individual
-vcftools --gzvcf merged_variants.vcf.gz --missing-indv
+vcftools --gzvcf P_chalceus_NP25_BarSW_merged.vcf.gz --missing-indv
 
-vcftools --gzvcf merged_variants.vcf.gz --max-missing 0.9 --minQ 30 --maf 0.05 --remove-indels --recode --stdout | bgzip > gwas_filtered.vcf.gz
+# Filtering
+vcftools --gzvcf P_chalceus_NP25_BarSW_merged.vcf.gz \
+--max-missing 0.9 --minQ 30 --maf 0.05 --remove-indels --recode --stdout | bgzip > P_chalceus_NP25_BarSW_merged_filtered.vcf.gz
 # max-missing 0.9: At least 90% of individuals must have a called genotype
 # minQ 30: minimum quality score of 30
 # maf 0.05: minor allele frequency (MAF) threshold of 0.05
 # remove-indels: exclude indels, keeping only SNPs
 
 # Index VCF file
-tabix -p vcf gwas_filtered.vcf.gz
+tabix -p vcf P_chalceus_NP25_BarSW_merged_filtered.vcf.gz
